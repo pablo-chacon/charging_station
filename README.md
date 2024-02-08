65bc6ae03d968c9446c29976945c35d5fe2067c0
pablo <ekarlsson66@gmail.com>
2024-02-08 22:41:40 +0100

    row 140-159

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ Assignment to integrate with a server to get electricy prising/consumption and s
 Python, JSON, Battery Managment System.
 
 In the cli client option 7 don't work.
-I have 2 different plot scripts. not that both is needed. I intend to implement machine learning so the program will be abla to optimize automatically.  
+in ChargingStation.py row 140-159 is commented out. 
+I have 2 different plot scripts. not that both is needed. I intend to implement machine learning through jupyter-notebook so the program will be able to optimize charging automatically.  
--- energy_plot.py
+++ energy_plot.py
@@ -1,8 +1,11 @@
 import requests as r
 import json
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+#import seaborn as sns
+import warnings 
+warnings.filterwarnings('ignore')
 # Plotting the price per hour and the consumption per hour.
 
 
@@ -11,10 +14,8 @@ consumption_data = r.get("http://127.0.0.1:5000/baseload").json()
 price_hour = range(0, len(price_data))
 price_data = pd.DataFrame(price_data)
 consumption_data = pd.DataFrame(consumption_data)
-
+price_data.head()
 fig, axs = plt.subplots()
 axs.plot(price_hour, price_data)
 axs.plot(price_hour, consumption_data)
-axs.plot(price_hour, price_data, consumption_data)
-fig.suptitle('Consumption and price per hour')
-plt.show()
+fig.suptitle('Pricing and Consumption')
